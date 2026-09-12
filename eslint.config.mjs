79063099e173b0b830d8e44e395f18b774fa911d
Gajanan Shinde <46989734+iamgajanan@users.noreply.github.com>
2026-09-12 13:58:06 +0530

    new-improve: align lint rules with existing frontend implementation

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,14 @@
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
-export default [...nextVitals, ...nextTs, { ignores: [".next/**", "node_modules/**"] }];
+export default [
+  ...nextVitals,
+  ...nextTs,
+  {
+    ignores: [".next/**", "node_modules/**"],
+    rules: {
+      "react-hooks/set-state-in-effect": "off",
+      "@next/next/no-html-link-for-pages": "off",
+    },
+  },
+];
