acdc04db20cf1257a654b3666680105c1ef3259e
Gajanan Shinde <46989734+iamgajanan@users.noreply.github.com>
2026-09-12 13:53:05 +0530

    new-improve: fix eslint flat config circular failure

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,4 @@
-import { dirname } from "node:path";
-import { fileURLToPath } from "node:url";
-import { FlatCompat } from "@eslint/eslintrc";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-const compat = new FlatCompat({ baseDirectory: __dirname });
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    ignores: [".next/**", "node_modules/**"],
-  },
-];
-
-export default eslintConfig;
+export default [...nextVitals, ...nextTs, { ignores: [".next/**", "node_modules/**"] }];
